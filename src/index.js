199390ef6f5bacae4f90707835e88f080574d02a
Kevin <kevingonzalezcuenca@gmail.com>
2022-12-24 17:07:36 +0100

    feat: add downloads catalogs

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {
 } from "./helpers/helpers.js"
 
 const navbarMenu = document.querySelector('.navbar__menu')
-const navbarLikns = document.querySelector('.navbar__links')
+const navbarLikns = document.querySelector('.navbar__container-links')
 const navbarClose = document.querySelector('.navbar__close-icon')
 
 const form = document.querySelector('.form')
@@ -18,12 +18,12 @@ const inputName = document.querySelector('form__input--name')
 const inputPhone = document.querySelector('form__input--phone')
 
 const resizeObserver = new ResizeObserver(() => {
-    navbarLikns.style.display = window.innerWidth >= WIDTH_DESKTOP ? 'block' : 'none'
+    navbarLikns.style.display = window.innerWidth >= WIDTH_DESKTOP ? 'flex' : 'none'
 })
 
 const openMenuMobile = () => {
     navbarLikns.classList.add('navbar__links-open')
-    navbarLikns.style.display = 'flex'
+    navbarLikns.style.display = 'block'
 }
 
 const closeMenuMobile = () => {
