87b589d1b804b022e6f8618149bde0cedc8c7dc7
Kevin <kevingonzalezcuenca@gmail.com>
2022-12-31 17:45:17 +0100

    feat: add validation inputs form contact

--- src/index.js
+++ src/index.js
@@ -15,9 +15,10 @@ const navbarLikns = document.querySelector('.navbar__container-links')
 const navbarClose = document.querySelector('.navbar__close-icon')
 
 //Form
-const form = document.querySelector('.form')
+const form = document.querySelector('.form__cta-link')
 const inputName = document.querySelector('.form__input--name')
 const inputPhone = document.querySelector('.form__input--phone')
+const inputEmail = document.querySelector('.form__input--email')
 const textareaMessage = document.querySelector('.form__message')
 
 //Gallery
@@ -46,38 +47,75 @@ const openModalGallery = (event) => {
     }
 }
 
+const createMessageError = (text) => {
+    const elementError = document.createElement('p')
+    elementError.setAttribute('class', 'form__input--error')
+    elementError.textContent = text
+    return elementError
+}
+
 const checkInputValid = (text, type) => {
+
+    console.log(textareaMessage.value.length);
+
     switch (type) {
         case TypeValidation.NAME:
-            return /[A-Za-z]/.test(text);
+            return /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/.test(text);
         case TypeValidation.PHONE:
             return /[0-9]{9}/.test(text);
+        case TypeValidation.EMAIL:
+            return /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/.test(text);
         case TypeValidation.MESSAGE:
-
-            if (textareaMessage.value.length <= 20) {
-                return false
-            }
-            return true;
+            return textareaMessage.value.length <= 20 ? false : true;
     }
 }
 
-const submitForm = (event) => {
-    event.preventDefault()
 
+
+const submitForm = (event) => {
 
     const isValidName = checkInputValid(inputName.value, TypeValidation.NAME)
     const isValidPhone = checkInputValid(inputPhone.value, TypeValidation.PHONE)
+    const isValidMessage = checkInputValid(textareaMessage.value, TypeValidation.MESSAGE)
+    const isValidEmail = checkInputValid(inputEmail.value, TypeValidation.EMAIL)
 
-    if (isValidName && isValidPhone) {
-        form.submit()
+    inputName.style.border = !isValidName ? '1px solid red' : '1px solid #ccc'
+    inputPhone.style.border = !isValidPhone ? '1px solid red' : '1px solid #ccc'
+    textareaMessage.style.border = !isValidMessage ? '1px solid red' : '1px solid #ccc'
+    inputEmail.style.border = !isValidEmail ? '1px solid red' : '1px solid #ccc'
+
+    if (isValidName && isValidPhone && isValidMessage && isValidEmail) {
+        fetch("https://formsubmit.co/ajax/info@rotulosarnao.es", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name: inputName.value,
+                phone: inputPhone.value,
+                message: textareaMessage.value,
+                email: inputEmail.value
+            })
+        }).then(data => {
+            if (data.status === 200) {
+                const modal = new bootstrap.Modal(document.getElementById('modalEmail'))
+                modal.show()
+                setTimeout(() => {
+                    modal.hide()
+                }, 4000)
+            }
+        })
+            .catch(error => console.error(error))
     }
+
 }
 
 navbarMenu.addEventListener('click', openMenuMobile)
 navbarClose.addEventListener('click', closeMenuMobile)
 
 document.addEventListener('scroll', closeMenuMobile)
-form.addEventListener("submit", submitForm)
+form.addEventListener("click", submitForm)
 resizeObserver.observe(document.body)
 
 printCardServices()
