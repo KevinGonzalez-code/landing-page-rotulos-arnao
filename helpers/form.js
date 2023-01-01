9033be10e2dcfd37febf89353bd492758df230c5
Kevin <kevingonzalezcuenca@gmail.com>
2023-01-01 16:00:44 +0100

    feat: add control errors when fetch form contact

--- helpers/form.js
+++ helpers/form.js
@@ -5,6 +5,20 @@ const inputPhone = document.querySelector('.form__input--phone')
 const inputEmail = document.querySelector('.form__input--email')
 const textareaMessage = document.querySelector('.form__message')
 
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        name: inputName.value,
+        phone: inputPhone.value,
+        message: textareaMessage.value,
+        email: inputEmail.value
+    })
+}
+
 const checkInputValid = (text, type) => {
     switch (type) {
         case TypeValidation.NAME:
@@ -18,30 +32,28 @@ const checkInputValid = (text, type) => {
     }
 }
 
+const changeTextErrorModal = () => {
+    document.getElementById('title-modal').innerHTML = 'Error, no se a podido enviar el correo'
+    document.getElementById('copy-modal').innerHTML = 'Por favor intentelo de nuevo mas tarde, disculpe las molestias.'
+}
+
 const fetchSendEmail = () => {
-    fetch("https://formsubmit.co/ajax/info@rotulosarnao.es", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            name: inputName.value,
-            phone: inputPhone.value,
-            message: textareaMessage.value,
-            email: inputEmail.value
-        })
-    }).then(data => {
-        if (data.status === 200) {
-            const modal = new bootstrap.Modal(document.getElementById('modalEmail'))
-            modal.show()
-            document.querySelector('.form').reset()
+    const modal = new bootstrap.Modal(document.getElementById('modalEmail'))
+
+    fetch("https://formsubmit.co/ajax/info@rotulosarnao.es", requestOptions)
+        .then(({ status }) => {
+            if (status === 200) {
+                modal.show()
+                document.querySelector('.form').reset()
+            } else {
+                changeTextErrorModal()
+                modal.show()
+            }
+        }).finally(() => {
             setTimeout(() => {
                 modal.hide()
             }, 4000)
-        }
-    })
-        .catch(error => console.error(error))
+        })
 }
 
 export const submitForm = (event) => {
@@ -57,7 +69,8 @@ export const submitForm = (event) => {
     inputEmail.style.border = !isValidEmail ? '1px solid red' : '1px solid #ccc'
 
     if (isValidName && isValidPhone && isValidMessage && isValidEmail) {
-        fetchSendEmail()       
+        fetchSendEmail()
     }
 
 }
+
